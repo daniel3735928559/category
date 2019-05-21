01a07e439a927f997804efb75596040ff0d078f2
DR <daniel3735928559@gmail.com>
2019-05-20 22:57:31 -0400

    maths

--- page/plugins/math.js
+++ page/plugins/math.js
@@ -3,32 +3,39 @@ class MathPlugin {
 	var self = this;
 	this.current_id = 0;
 	this.docs = {};
+	this.snippets = {};
 	this.index = {}; // Will store {node_id:{element_name:[formula tags containing element]}}
 	Guppy.init({"path":"/node_modules/guppy-js","symbols":"/node_modules/guppy-js/sym/symbols.json"});
 	Vue.component('math-plugin', {
 	    template: `
 <span class="category-math-plugin">
 <a v-bind:name="'category-math-plugin-link-'+id"></a>
-  <span class="category-math-plugin-math" v-on:click="display_syms = !display_syms" v-html="rendered"></span>
-  <div class="category-math-plugin-vars" v-if="display_syms">
-  Vars:
-  <ul>
-    <li v-for="e in syms">
-      <a href="#" v-on:click="query = e">{{e}}</a>
+  <span v-bind:id="'category-math-plugin-expr-'+id" class="category-math-plugin-math" v-on:click="display_syms = !display_syms" v-html="rendered"></span>
+  <div class="category-math-plugin-vars" v-bind:style="get_pos(id)" v-if="display_syms">
+    <a href="#" v-on:click="display_syms = false; query = ''">[x]</a>
+    Vars:
+    <ul>
+      <li v-for="e in syms">
+        <a href="#" v-on:click="query = e">{{e}}</a>
     </li>
-  </ul>
-  </div>
-  <div class="category-math-plugin-vars" v-if="query != ''">
-  <a href="#" v-on:click="query = ''">[x]</a>
-  Uses:
-  <ul>
-    <li v-for="x in master.index[query]">
-      <a v-bind:href="'#category-math-plugin-link-'+x">{{x}}</a>
-    </li>
-  </ul>
+    </ul>
+    <div class="category-math-plugin-refs" v-if="display_syms && query != ''">
+    Uses:
+    <ul>
+      <li v-for="x in master.index[query]">
+        <a v-bind:href="'#category-math-plugin-link-'+x">{{master.snippets[x]}}</a>
+      </li>
+    </ul>
+    </div>
   </div>
 </span>`,
 	    props: ["query","syms","id","display_syms","rendered","master"],
+	    methods: {
+		get_pos: function(id) {
+		    var el = document.getElementById(id);
+		    return {'position':'absolute','left':'100%','top':0,'max-width':'50%'};
+		}
+	    }
 	});
     }
     run(comp, node, root, index){
@@ -41,6 +48,8 @@ class MathPlugin {
 	this.docs[node][index] = res.doc.get_vars().concat(res.doc.get_symbols());
 	var rendered_content = (new XMLSerializer()).serializeToString(res.container);
 	var container = document.createElement("span");
+
+
 	
 	// Put this doc ID in the index for each var and symbol in the document
 	for(var i = 0; i < this.docs[node][index].length; i++) {
@@ -48,6 +57,22 @@ class MathPlugin {
 	    if (!this.index[v]) this.index[v] = [];
 	    if (this.index[v].indexOf(doc_id) < 0) this.index[v].push(doc_id);
 	}
+
+	var snippet = "";
+	if(root.previousSibling){
+	    snippet += root.previousSibling.textContent.split(" ").slice(-4).join(" ");
+	}
+	snippet += " [formula] "
+
+	if(root.nextSibling) {
+	    snippet += root.nextSibling.textContent.split(" ").slice(0,4).join(" ");
+	}
+	snippet = "..." + snippet + "...";
+	console.log("parprev",root.parentNode.previousSibling);
+	console.log("parnext",root.parentNode.nextSibling);
+
+	this.snippets[doc_id] = snippet;
+	
         root.parentNode.insertBefore(container, root);
 
 	new comp.$options.components['math-plugin']({
