15c873a20b8c9632808e2fcedee34aa565453d8e
DR <daniel3735928559@gmail.com>
2019-06-23 02:05:25 -0400

    parallelisation

--- src/build.py
+++ src/build.py
@@ -1,10 +1,44 @@
 """Usage: build.py <input_dir> <output_dir> [rebuild]"""
 
-import docopt, os, traceback
+import docopt, os, traceback, copy
+from multiprocessing import Process, Queue
 from . util import *
 from . mdbuild import md_builder
 from . xmlbuild import xml_builder
 from . plugins import *
+
+NUM_WORKERS=20
+
+def build_worker(inputs, outputs):
+    for input_dir, output_dir, fn, extra_edges, md_time in iter(inputs.get, 'STOP'):
+        ans = {'empty':True}
+        ending = fn[fn.rfind('.'):]
+        endings = {".md":md_builder, ".xml":xml_builder}
+        if ending in endings:
+            ans['empty'] = False
+            common_prefix = os.path.commonprefix([input_dir, fn])
+            src_path = os.path.relpath(fn, common_prefix)
+            if os.path.getmtime(fn) < md_time:
+                ans['error'] = "Already up-to-date: {}".format(fn)
+            else:
+                print("PROCESSING",fn)
+                args = {}
+                if ending == ".md":
+                    args = {"plugins":{"slideshow":"md","math":{},"video":video.VideoPlugin(),"link":"txt","query":"txt"}}
+                builder = endings[ending](fn,output_dir,**args)
+                if not builder.OK:
+                    ans['error'] = "WARNING: There was a problem building {}".format(fn)
+                elif not 'name' in builder.node:
+                    ans['error'] = "WARNING: No name found in {}".format(fn)
+                else:
+                    if not 'edges' in builder.node:
+                        builder.node['edges'] = {'has':{},'is':{}}
+                    builder.node['src'] = src_path
+                    edges = builder.node['edges']
+                    add_edges(extra_edges, edges)
+                    ans['error'] = None
+                    ans['builder'] = builder
+        outputs.put(ans)
 
 class cat_builder:
     def __init__(self, input_dir, output_dir, force_rebuild=False):
@@ -15,8 +49,9 @@ class cat_builder:
             self.metadata = import_metadata(md_file)
             self.md_time = os.path.getmtime(md_file)
 
-        # The endings we understand and the classes that can read these files
-        self.endings = {".md":md_builder, ".xml":xml_builder}
+        input_queue = Queue()
+        output_queue = Queue()
+        num_inputs = 0
 
         # The name of the metadata file that that will contain edges for all nodes in a subdirectory
         self.metadata_conf = "metadata.conf"
@@ -49,42 +84,50 @@ class cat_builder:
             # Now read the actual data files
             for fn in files:
                 fn = os.path.join(dirname, fn)
-                ending = fn[fn.rfind('.'):]
-                if ending in self.endings:
-                    common_prefix = os.path.commonprefix([input_dir, fn])
-                    src_path = os.path.relpath(fn, common_prefix)
-                    if src_path in srcs and os.path.getmtime(fn) < self.md_time:
-                        print("Already up-to-date: ",fn)
-                        continue
-                    print("PROCESSING",fn)
-                    args = {}
-                    if ending == ".md":
-                        args = {"plugins":{"slideshow":"md","math":{},"video":video.VideoPlugin(),"link":"txt","query":"txt"}}
-                    builder = self.endings[ending](fn,output_dir,**args)
-                    if not builder.OK:
-                        print("WARNING: There was a problem building",fn)
-                        continue
-                    if not 'name' in builder.node:
-                        print("WARNING: No name found in",fn)
-                        continue
-                    if builder.ID in self.metadata:
-                        es = "ERROR: Two nodes with the same name: {}\nSrc: {}\nSrc: {}\n"
-                        print(es.format(builder.node['name'], self.metadata[builder.ID]['src'], src_path))
-                        return
-                    self.metadata[builder.ID] = builder.node
-                    self.metadata[builder.ID]['src'] = src_path
-                    if not 'edges' in self.metadata[builder.ID]: self.metadata[builder.ID]['edges'] = {'has':{},'is':{}}
-                    edges = self.metadata[builder.ID]['edges']
-                    add_edges(self.config_edges, edges)
-                    with open(os.path.join(output_dir,'{}.html'.format(builder.ID)),"wb") as f:
-                        f.write(builder.doc)
+                input_queue.put([input_dir, output_dir, fn, copy.deepcopy(self.config_edges), self.md_time])
+                num_inputs += 1
+
+        # Now start the workers
+        for i in range(NUM_WORKERS):
+            Process(target=build_worker, args=(input_queue, output_queue)).start()
+
+        for i in range(NUM_WORKERS):
+            input_queue.put('STOP')
             
+        # The results should come in output_queue
+        for i in range(num_inputs):
+            ans = output_queue.get()
+            if ans['empty']: continue
+            elif not ans['error'] is None:
+                print("E:",ans['error'])
+                continue
+            builder = ans['builder']
+            
+            # Ensure no ID collisions
+            if builder.ID in self.metadata:
+                es = "ERROR: Two nodes with the same name: {}\nSrc: {}\nSrc: {}\n"
+                print(es.format(builder.node['name'], self.metadata[builder.ID]['src'], src_path))
+                return
+
+            # Get the path of the file to write the processed document to
+            output_path = os.path.join(output_dir,'{}.html'.format(builder.ID))
+            print("WRITING {}: {} -> {}".format(builder.node['name'], builder.node['src'], output_path))
+
+            # Add the data to our metadata dictionary
+            self.metadata[builder.ID] = builder.node
+
+            # Actually write the output document
+            with open(output_path,"wb") as f:
+                f.write(builder.doc)
+
+        
         self.metadata = complete_metadata(self.metadata)
         for x in self.metadata:
             print("MD",x,self.metadata[x])
         with open(os.path.join(output_dir,'metadata.json'),"w") as f:
             f.write(jsonenc().encode(self.metadata))
 
+        
 if __name__ == "__main__":
     args = docopt.docopt(__doc__)
     cat_builder(args['<input_dir>'], args['<output_dir>'],args['rebuild'])
